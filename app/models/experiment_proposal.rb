204183e980a1c56b87e8311c5adfc40ceaa410da
Alex Camacho <email": null>
2017-06-01 16:44:14 -0400

    Removed commented out code

--- app/models/experiment_proposal.rb
+++ app/models/experiment_proposal.rb
@@ -1,5 +1,4 @@
 class ExperimentProposal < ApplicationRecord
-  # belongs_to :proposer, class_name: "User"
   belongs_to :user
   has_many :experiments, dependent: :destroy
   has_many :comments, as: :commentable
